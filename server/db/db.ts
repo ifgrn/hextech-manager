1b960d4ebf97c42df2fabbe491cecf6466787eda
ifgrn <adbringas07@gmail.com>
2025-12-24 15:37:23 -0500

    hextech db created

---
+++ server/db/db.ts
@@ -0,0 +1,46 @@
+import { Database } from "bun:sqlite";
+
+const db = new Database("hextachdb.sqlite", { create: true });
+db.run("PRAGMA foreign_keys = ON");
+
+function initalizeDatabase(): void {
+  try {
+    db.run(`
+      DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS lol_accounts;
+      `);
+
+    db.run(`CREATE TABLE IF not EXISTS users (
+      id TEXT primary key default (lower(hex(randomblob(16)))),
+      username text unique not null,
+      email text unique not null,
+      password_hash text not null,
+      created_at timestamp default current_timestamp,
+      updated_at timestamp default current_timestamp,
+      is_active boolean default 1,
+      is_admin boolean default 0
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS lol_accounts(
+      id integer primary key autoincrement,
+      owner_id text not null,
+      gameName text not null,
+      tagLine text not null,
+      puuid text not null,
+      acc_user text not null,
+      password text not null,
+      server text not null,
+      is_active boolean default 1,
+      created_at timestamp default current_timestamp,
+      updated_at timestamp default current_timestamp,
+      foreign key (owner_id) references users(id)
+    )`);
+  } catch (e) {
+    console.error("❌ Error inicializando la base de datos:", e);
+    throw new Error();
+  }
+}
+
+initalizeDatabase();
+
+export { db };
