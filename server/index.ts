9528539bd86e560def7c8fd5a4f0d02eddef72c2
ifgrn <adbringas07@gmail.com>
2025-12-26 23:12:21 -0500

    Cors added

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,14 @@ import { Hono } from "hono";
 import users from "./routes/user-routes";
 import accounts from "./routes/accounts-routes";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 
 const port = process.env.PORT;
 const app = new Hono();
 
 app.use(logger());
+
+app.use("/", cors());
 
 app.route("/api/v1/users", users);
 app.route("/api/v1/accounts", accounts);
