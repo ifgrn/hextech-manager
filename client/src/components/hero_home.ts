8dc318e5922d8bdaeb854f40dbb69c54247c949a
ifgrn <adbringas07@gmail.com>
2026-01-06 14:00:47 -0500

    reset front

--- client/src/components/hero_home.ts
+++
@@ -1,8 +0,0 @@
-export const hero_home = () => {
-  return `
-  <section class="hero-container">
-    <h1>The most complete way to manage your League accounts.</h1>
-    <a class="">Get Started <img src="/arrow.svg"/></a>
-  </section>
-  `;
-};
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,24 @@
-import "./style.css";
-import { home_view } from "./views/home_view";
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
 
-const $ = (element: string) => document.querySelector(element);
-const $app = $("#app");
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
 
-$app.innerHTML = home_view();
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
